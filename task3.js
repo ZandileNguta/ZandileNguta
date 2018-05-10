e99dd1ed6484be2e7169a93e878c794e0188acb8
ZandileNguta <zandilelendiza869@gmail.com>
2018-05-10 13:08:39 +0200

    This is My First Commit

---
+++ task3.js
@@ -0,0 +1,10 @@
+var zandyBox1 = document.getElementById('box1');
+var zandyBox = document.getElementById('box2');
+var greenButton = document.getElementById('green');
+
+greenButton.addEventListener('click', function(){
+zandyBox1.style.backgroundColor = 'green'
+})
+greenButton.addEventListener('click', function(){
+zandyBox.style.backgroundColor = 'red'
+})
---
+++ task4.js
@@ -0,0 +1,12 @@
+var zandyBox = document.getElementById('box1');
+var greenButton = document.getElementById('green');
+var redButton = document.getElementById('red');
+
+
+greenButton.addEventListener('click', function(){
+zandyBox.style.backgroundColor = 'green'
+})
+redButton.addEventListener('click', function(){
+zandyBox.style.backgroundColor = 'red'
+})
+
